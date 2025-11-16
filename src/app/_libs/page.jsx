a963ea98762551bb1e1a99d8920fc98b044e1f7d
Mr-BlackHat-lab <prahalad.hat@outlook.com>
2025-11-17 01:20:21 +0530

    private folder

---
+++ src/app/_libs/page.jsx
@@ -0,0 +1,7 @@
+export default function Privatepage(){
+    return (
+        <>
+        <h1>you cant view this page in browser because the file or parent folder name start with '_' underscore</h1>
+        </>
+    )
+}
