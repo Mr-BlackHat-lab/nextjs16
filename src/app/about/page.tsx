ad0bdc854bfcbaf3ee45d7989a375d3af1b7712b
Mr-BlackHat-lab <prahalad.hat@outlook.com>
2025-11-17 03:20:47 +0530

    meta data in progress

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,7 @@
+export const metadata = {
+    title : "about learn"
+    
+}
 export default function About(){
     return(
         <h1>This is About me !!</h1>
--- src/app/products/[productid]/page.tsx
+++ src/app/products/[productid]/page.tsx
@@ -1,8 +1,14 @@
-export default async function Productdetails(
-    {params}:{params:Promise<{productid:string}>;}
-) {
-    const productid=(await params).productid;
-    return(
-        <h1>Details of product {productid}</h1>
-    )
+import { Metadata } from "next";
+type Props = {
+  params: Promise<{ productid: string }>;
+};
+export const genreateMetadata = async ({params}:Props):Promise<Metadata>=>{
+    const id = (await params).productid;
+    return {
+        title : `product ${id} details page`
+    }
+}
+export default async function Productdetails({ params }: Props) {
+  const productid = (await params).productid;
+  return <h1>Details of product {productid}</h1>;
 }
