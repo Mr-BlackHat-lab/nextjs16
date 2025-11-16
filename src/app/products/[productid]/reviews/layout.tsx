c8c52f50e92960e23c4078b08ed98ac6a9927741
Mr-BlackHat-lab <prahalad.hat@outlook.com>
2025-11-17 01:53:12 +0530

    nested layout

---
+++ src/app/products/[productid]/reviews/layout.tsx
@@ -0,0 +1,12 @@
+export default function ProductDetailslayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      {children}
+      <h2> also dont buy any other shits</h2>
+    </>
+  );
+}
