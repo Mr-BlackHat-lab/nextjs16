e7a5a9defa014cfa4ae3e6e65d85790b082c9445
Mr-BlackHat-lab <prahalad.hat@outlook.com>
2025-11-17 12:29:43 +0530

    generateMetadat

--- src/app/products/[productid]/page.tsx
+++ src/app/products/[productid]/page.tsx
@@ -2,12 +2,19 @@ import { Metadata } from "next";
 type Props = {
   params: Promise<{ productid: string }>;
 };
-export const genreateMetadata = async ({params}:Props):Promise<Metadata>=>{
-    const id = (await params).productid;
-    return {
-        title : `product ${id} details page`
-    }
-}
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const id = (await params).productid;
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`${id} shit product`);
+    }, 100);
+  });
+  return {
+    title: `product ${title} details page`,
+  };
+};
 export default async function Productdetails({ params }: Props) {
   const productid = (await params).productid;
   return <h1>Details of product {productid}</h1>;
