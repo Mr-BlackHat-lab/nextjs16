e7b339b75e7a531e164a4245419f1d8cf062c24c
Mr-BlackHat-lab <prahalad.hat@outlook.com>
2025-11-17 17:48:24 +0530

    recovering from error

--- src/app/products/[productid]/reviews/[reviewid]/error.tsx
+++ src/app/products/[productid]/reviews/[reviewid]/error.tsx
@@ -1,9 +1,33 @@
 "use client";
+<<<<<<< HEAD
 export default function ErrorBoundary({ error }: { error: Error }) {
   return (
     <div>
       <h1>{error.message}</h1>
       {/*<h1>Error in review id (randomly gentrated error)</h1>*/}
+=======
+import { useRouter } from "next/navigation";
+import { startTransition } from "react";
+export default function ErrorBoundary({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  const router = useRouter();
+  const reload = () => {
+    startTransition(() => {
+      router.refresh();
+      reset();
+    });
+  };
+  return (
+    <div>
+      <h1>{error.message}</h1>
+      <button onClick={() => reload()}>Try again</button>
+      <h1>Error in review id (randomly gentrated error)</h1>
+>>>>>>> 2033a75 (recovering from error)
     </div>
   );
 }
