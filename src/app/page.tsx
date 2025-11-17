41dc05b7891e202fa33e4ab227f73c6ed12c4e86
Mr-BlackHat-lab <prahalad.hat@outlook.com>
2025-11-17 14:35:59 +0530

    link navgaition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
-
+import Link from "next/link";
 export default function Home() {
   return (
-  <h1>welcome Home!</h1>
+    <>
+      <h1>welcome Home!</h1>
+      <Link href="/blog">Blog</Link>
+      <Link href="/products">Product</Link>
+      <Link href="/about">About</Link>
+    </>
   );
 }
