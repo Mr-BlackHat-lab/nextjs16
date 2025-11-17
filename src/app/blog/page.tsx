b3bbfe74e44ec8c912cc68b073baf86c9d152016
Mr-BlackHat-lab <prahalad.hat@outlook.com>
2025-11-17 17:12:19 +0530

    loading tsx filse

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,6 +2,11 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Blog Main Page",
 };
-export default function blog() {
+export default async function blog() {
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("intentional delay");
+    }, 2000);
+  });
   return <h1>blog main page</h1>;
 }
