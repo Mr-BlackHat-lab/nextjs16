15445d69e3b84c6aa4db678bc206634d99d80028
Mr-BlackHat-lab <prahalad.hat@outlook.com>
2025-11-17 15:09:12 +0530

    active links in layout.tsx of auth and import tailwindcss in globals.css and import it in app/layout.tsx

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,35 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+const navlink = [
+  { name: "register", href: "/register" },
+  { name: "login", href: "/login" },
+  { name: "forgot password", href: "/forgot-password" },
+];
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const pathname = usePathname();
+  return (
+    <>
+      {navlink.map((link) => {
+        const isActive =
+          pathname === link.href ||
+          (pathname.startsWith(link.href) && link.href !== "/");
+        return (
+          <Link
+            className={isActive ? "font-bold mr-4" : "text-blue-500 mr-4"}
+            href={link.href}
+            key={link.name}
+          >
+            {link.name}
+          </Link>
+        );
+      })}
+      {children}
+    </>
+  );
+}
