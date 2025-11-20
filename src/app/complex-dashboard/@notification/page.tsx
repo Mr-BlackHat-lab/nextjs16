44f6f48585040f8a04666e5188ad4cf403a31cc2
Mr-BlackHat-lab <prahalad.hat@outlook.com>
2025-11-20 15:26:15 +0530

    in parller routes you need to add default js to work

--- src/app/complex-dashboard/@notification/page.tsx
+++ src/app/complex-dashboard/@notification/page.tsx
@@ -1,4 +1,13 @@
 import { Card } from "@/components/card";
+import Link from "next/link";
+
 export default function NotificationPage() {
-  return <Card>NotificationPage</Card>;
+  return (
+    <Card>
+      <div>NotificationPage</div>
+      <div>
+        <Link href="/complex-dashboard/archived">Archived</Link>
+      </div>
+    </Card>
+  );
 }
